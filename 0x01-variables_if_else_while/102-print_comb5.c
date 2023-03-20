dc63278bec5abe9dc174d1795663ea02075e445b
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-20 14:32:57 -0700

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * main - print 2 digit combination from 00 - 99
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+
+	for (a = 0; a < 58; a++)
+	{
+		for (b = 0; b < 58; b++)
+		{
+			for (c = 0; c < 58; c++)
+			{
+				for (d = 1; d < 58; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(' ');
+					putchar(c);
+					putchar(d);
+					if ((a == 57) && (b == 56) && (c == 57) && (d == 57))
+					{
+					}
+					else
+					{
+						putchar(',');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
