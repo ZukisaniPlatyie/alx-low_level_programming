f7e7f272f8b4da78c3ce51a87b1c22ad5b6d869d
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-20 05:24:39 -0700

    3. alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabetg in lower and upper case
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		putchar(l);
+	}
+	for (l = 'A'; l <= 'Z'; l++)
+	{
+		putchar(l);
+	}
+	putchar('\n');
+	return (0);
+}
