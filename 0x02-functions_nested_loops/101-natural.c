298d3887f801c479ae80063bc10d70471613d69f
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-23 04:45:24 -0700

    13. Nature made the natural numbers; All else is the work of women

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main: prints the sum of all multiples of 3 or 5 up to 1024
+ * main - prints the sum of all multiples of 3 or 5 up to 1024
+ *
  * Return: 0 always
  */
 
