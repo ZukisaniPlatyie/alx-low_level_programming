4a2f171d92641cfad7deb1588f5de03b72c33e74
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-23 05:20:20 -0700

    15. Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - prints the sum of even fibonacci numbers less than 400000
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a = 0;
+	long b = 1, c = 2, sum = c;
+
+	while (c + b < 400000)
+	{
+		c += b;
+		if (c % 2 == 0)
+		{
+			sum += c;
+			b = c - b;
+		}
+		a++;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
