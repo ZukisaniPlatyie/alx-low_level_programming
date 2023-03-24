f8c44491c4a8926e117552cc91d2f6e5b120c0a0
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-24 05:10:30 -0700

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle ff by \n
+ * @size: prints diagonals
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	int row, hash, spc;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spc = size - row; spc >= 1; spc--)
+			{
+				_putchar(' ');
+			}
+			for (hash = 1; hash <= row; hash++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+}
