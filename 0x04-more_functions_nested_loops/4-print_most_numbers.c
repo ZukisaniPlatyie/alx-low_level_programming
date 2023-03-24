34067ed5bd60b0625427660b4876125979081f38
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-24 01:14:01 -0700

    4. I believe in numbers and signs

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print the numbers from 0 to 9
+ * print ecluding 2 and 4
+ * Return: numbers from 0 to 9
+ */
+
+void print_most_numbers(void)
+{
+	int x = 0;
+
+	for (; x <= 9; x++)
+	{
+		if (x == 2 || x == 4)
+		{
+		}
+		else
+		{
+			_putchar(x + '0');
+		}
+	}
+	_putchar('\n');
+}
