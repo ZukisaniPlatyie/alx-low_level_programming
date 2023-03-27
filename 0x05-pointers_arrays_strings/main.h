61cadbc277d26342be0123ef0f983b51fb78b205
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-27 14:19:29 -0700

    2. This report, by its very length, defends itself against the risk of being read

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,7 +4,7 @@
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
-int _strlen(char *s);
+size_t _strlen(char *str);
 void _puts(char *str);
 void print_rev(char *s);
 void puts2(char *str);
