2aecba34ebc0e205c9dc9208bf0aebc861fa41a1
ZukisaniPlatyie <platyieza@gmail.com>
2023-03-27 17:15:23 -0700

    11. Don't hate the hacker, hate the code

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - generate random password
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	char psswrd[84];
+	int index = 0, sum = 0, diff_half1, diff_half2;
+
+	srand(time(0));
+
+	while (sum < 2772)
+	{
+		psswrd[index] = 33 + rand() % 94;
+		sum += psswrd[index++];
+	}
+	psswrd[index] = '\0';
+	if (sum != 2772)
+	{
+		diff_half1 = (sum - 2772) / 2;
+		diff_half2 = (sum - 2772) / 2;
+		if ((sum - 2772) % 2 != 0)
+		{
+			diff_half1++;
+		}
+		for (index = 0; psswrd[index]; index++)
+		{
+			if (psswrd[index] >= (33 + diff_half1))
+			{
+				psswrd[index] -= diff_half1;
+			}
+		}
+		for (index = 0; psswrd[index]; index++)
+		{
+			if (psswrd[index] >= (33 + diff_half2))
+			{
+				psswrd[index] -= diff_half2;
+			}
+		}
+	}
+	printf("%s", psswrd);
+	return (0);
+}
