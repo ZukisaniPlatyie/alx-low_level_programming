4a4933816f3de931a4a93a1e117c78e64c95d640
ZukisaniPlatyie <platyieza@gmail.com>
2023-04-05 13:16:09 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-main.c
+++ 0x08-recursion/6-main.c
@@ -28,3 +28,23 @@ int main(void)
 	printf("%d\n", r);
 	return (0);
 }
+
+/**
+ * actual_prime - calculates if a number is a prime number
+ * @n: number to be checked
+ * @i: iterator
+ * Return: 1 if prime, 0 if not
+ */
+
+int actual_prime(int n, int i)
+{
+	if (i == 1)
+	{
+		return (1);
+	}
+	if (n % i == 0 && i > 0)
+	{
+		return (0);
+	}
+	return (actual_prime(n, i - 1));
+}
