3001bc30d23ff415743e5f146f92bf880e696cbe
ZukisaniPlatyie <platyieza@gmail.com>
2023-04-05 11:56:29 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * _putchar - write the character to c
+ * @c: character to print
+ * Return: 1 on success
+ * On error, -1 is returned
+ *
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c. 1));
+}
