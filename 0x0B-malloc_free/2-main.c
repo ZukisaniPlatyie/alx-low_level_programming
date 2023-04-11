283a401e100448df5528218b82515348e5ed475f
ZukisaniPlatyie <platyieza@gmail.com>
2023-04-11 15:53:28 -0700

    4. It's not bragging if you can back it up

--- 0x0B-malloc_free/2-main.c
+++ 0x0B-malloc_free/2-main.c
@@ -4,20 +4,20 @@
 
 /**
  * main - check the code for ALX School students.
- *
  * Return: Always 0.
  */
+
 int main(void)
 {
-    char *s;
+	char *s;
 
-    s = str_concat("Betty ", "Holberton");
-    if (s == NULL)
-    {
-        printf("failed\n");
-        return (1);
-    }
-    printf("%s\n", s);
-    free(s);
-    return (0);
+	s = str_concat("Betty ", "Holberton");
+	if (s == NULL)
+	{
+		printf("failed\n");
+		return (1);
+	}
+       	printf("%s\n", s);
+	free(s);
+	return (0);
 }
