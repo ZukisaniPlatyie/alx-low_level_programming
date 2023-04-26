de555c7d00bed9197fab4452c8d7191024444011
ZukisaniPlatyie <platyieza@gmail.com>
2023-04-26 06:37:17 -0700

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/100-is_palindrome.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+int check_pal(char *s, int i, int len);
+int _strlen_recursion(char *s);
+
+/**
+ * is_palindrome - checks if a string is a palindrome
+ * @s: string to reverse
+ * Return: 1 if string is palindrome and 0 if not
+ */
+
+int is_palindrome(char *s)
+{
+	if (*s == 0)
+	{
+		return (1);
+	}
+	return (check_pal(s, 0, _strlen_recursion(s)));
+}
+
+/**
+ *
