3f43ba89f07cfab50c7c633a687eaedfc2c074b4
ZukisaniPlatyie <platyieza@gmail.com>
2023-04-26 07:02:11 -0700

    0x08. C - Recursion

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -2,19 +2,15 @@
 
 /**
  * factorial - returns the factorial of a number
- * @n: number to be returned and factorised
+ * @n: number to return the factorial from
+ *
  * Return: factorial of n
  */
-
 int factorial(int n)
 {
 	if (n < 0)
-	{
 		return (-1);
-	}
 	if (n == 0)
-	{
 		return (1);
-	}
-	retun(n * factorial(n -1));
+	return (n * factorial(n - 1));
 }
--- 0x08-recursion/6-main.c
+++ 0x08-recursion/6-main.c
@@ -3,48 +3,28 @@
 
 /**
  * main - check the code
+ *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int r;
+    int r;
 
-	r = is_prime_number(1);
-	printf("%d\n", r);
-	r = is_prime_number(1024);
-	printf("%d\n", r);
-	r = is_prime_number(16);
-	printf("%d\n", r);
-	r = is_prime_number(17);
-	printf("%d\n", r);
-	r = is_prime_number(25);
-	printf("%d\n", r);
-	r = is_prime_number(-1);
-	printf("%d\n", r);
-	r = is_prime_number(113);
-	printf("%d\n", r);
-	r = is_prime_number(7919);
-	printf("%d\n", r);
-	return (0);
-}
-
-/**
- * actual_prime - calculates if a number is a prime number
- * @n: number to be checked
- * @i: iterator
- * Return: 1 if prime, 0 if not
- */
-
-int actual_prime(int n, int i)
-{
-	if (i == 1)
-	{
-		return (1);
-	}
-	if (n % i == 0 && i > 0)
-	{
-		return (0);
-	}
-	return (actual_prime(n, i - 1));
+    r = is_prime_number(1);
+    printf("%d\n", r);
+    r = is_prime_number(1024);
+    printf("%d\n", r);
+    r = is_prime_number(16);
+    printf("%d\n", r);
+    r = is_prime_number(17);
+    printf("%d\n", r);
+    r = is_prime_number(25);
+    printf("%d\n", r);
+    r = is_prime_number(-1);
+    printf("%d\n", r);
+    r = is_prime_number(113);
+    printf("%d\n", r);
+    r = is_prime_number(7919);
+    printf("%d\n", r);
+    return (0);
 }
